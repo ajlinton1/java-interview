d47ec10e392be89c3c86d444cd4eeca884c8e32b
ajlinton1 <ajlinton@hotmail.com>
2022-03-24 07:06:57 -0700

    Robot grid

--- src/test/java/chapter8/RobotGridTest.java
+++ src/test/java/chapter8/RobotGridTest.java
@@ -61,4 +61,51 @@ public class RobotGridTest {
             this.y = y;
         }
     }
+
+    @Test
+    public void testComputePath1() {
+        boolean[][] maze = new boolean[3][3];
+
+        maze[1][1] = true;
+        maze[2][1] = true;
+
+        Set<Point> result = new LinkedHashSet<>();
+        Boolean[][] cache = new Boolean[3][3];
+        var pathFound = computePath1(2, 2, maze, result, cache);
+
+        Point[] finalResult = new Point[result.size()];
+
+        int pos = finalResult.length - 1;
+        for (Point point: result) {
+            finalResult[pos--] = point;
+        }
+
+        for (int i = 0; i < finalResult.length; i++) {
+            System.out.println(String.format("%d %d", finalResult[i].x, finalResult[i].y));
+        }
+
+        assertTrue(pathFound);
+    }
+
+    boolean computePath1(int m, int n, boolean[][] maze, Set<Point> path, Boolean[][] cache) {
+        if (m < 0 || n < 0) {
+            return false;
+        }
+        if (cache[m][n] != null) {
+            return cache[m][n];
+        }
+        if (maze[m][n]) {
+            cache[m][n] = false;
+            return false;
+        }
+        if (((m == 0) && (n == 0))
+                || computePath1(m, n -1, maze, path, cache)
+                || computePath1(m - 1, n, maze, path, cache)) {
+            path.add(new Point(m, n));
+            cache[m][n] = true;
+            return true;
+        }
+        cache[m][n] = false;
+        return false;
+    }
 }
